20ffdd379c6e209d3366bc9833043f24a93b0cfc
Kamal Khan <shout@bhittani.com>
2016-11-23 13:28:05 +0500

    TDD Repository.

---
+++ src/Repository_2_0_0.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Boots;
+
+/**
+ * Boots repository class.
+ *
+ * @package Boots
+ * @subpackage Repository
+ * @version 2.0.0
+ * @see http://wpboots.com
+ * @link https://github.com/wpboots/boots
+ * @author Kamal Khan <shout@bhittani.com> https://bhittani.com
+ * @license https://github.com/wpboots/boots/blob/master/LICENSE
+ * @copyright Copyright (c) 2014-2016, Kamal Khan
+ */
+
+// Die if accessing this script directly.
+if(!defined('ABSPATH')) die(-1);
+
+/**
+ * @package Boots
+ * @subpackage Repository
+ * @version 2.0.0
+ */
+class Repository_2_0_0
+{
+    /**
+     * The repository
+     * @var array
+     */
+    protected $repo = [];
+
+    /**
+     * The delegates
+     * @var array
+     */
+    protected $delegates = [];
+
+    /**
+     * Construct with an optional array.
+     * @param array $repo The initial repository array
+     */
+    public function __construct(array $repo = [])
+    {
+        $this->repo = $repo;
+    }
+
+    /**
+     * Resolve a dot notated key from the repository.
+     * @param  string  $key   Key to resolve
+     * @param  boolean $found Was the key found or not?
+     * @return mixed   Value
+     */
+    protected function resolve($key, & $found = false)
+    {
+        $keys = explode('.', $key);
+        $repo = $this->repo;
+        foreach ($keys as $k) {
+            if (!is_array($repo)
+                || !array_key_exists($k, $repo)) {
+                foreach ($this->delegates as $delegate) {
+                    $repo = $delegate->resolve($key, $found);
+                    if ($found) {
+                        return $repo;
+                    }
+                }
+                $found = false;
+                return;
+            }
+            $repo = $repo[$k];
+        }
+        $found = true;
+        return $repo;
+    }
+
+    /**
+     * Get the entire repository as an array.
+     * @param  boolean $withDelegates Include delegates?
+     * @return array   Repository array with preferable delegates
+     */
+    public function all($withDelegates = false)
+    {
+        if ($withDelegates) {
+            $repos = [];
+            $repos[] = $this->repo;
+            foreach ($this->delegates as $delegate) {
+                $repos[] = $delegate->all();
+            }
+            return $repos;
+        }
+        return $this->repo;
+    }
+
+    /**
+     * Get the entire repository array with delegates.
+     * @return array Repository array with delegates
+     */
+    public function everything()
+    {
+        return $this->all(true);
+    }
+
+    /**
+     * Check to see if the given key exists in the repository.
+     * @param  string  $key Key string to check for
+     * @return boolean Exists or not
+     */
+    public function has($key)
+    {
+        $has = false;
+        $this->resolve($key, $has);
+        return $has;
+    }
+
+    /**
+     * Get the value for a given key.
+     * @param  string $key     Key string
+     * @param  mixed  $default Default value if key isn't found
+     * @return mixed  Value for the given key
+     */
+    public function get($key, $default = null)
+    {
+        $found = false;
+        $value = $this->resolve($key, $found);
+        if (!$found) {
+            return $default;
+        }
+        return $value;
+    }
+
+    /**
+     * Set a value for a given key.
+     * @param string     $key   Key string
+     * @param mixed      $value Value to set
+     * @param array|null $repo  For internal use
+     * @return $this Allow chaining
+     */
+    public function set($key, $value, & $repo = null)
+    {
+        $keys = explode('.', $key);
+        $key = array_shift($keys);
+        if (count($keys) == 0) {
+            if (is_null($repo)) {
+                $repo = [$key => $value];
+                $this->repo = array_replace_recursive($this->repo, $repo);
+                return $this;
+            }
+            $repo[$key] = $value;
+            return $this;
+        }
+        $repo[$key] = [];
+        $this->set(implode('.', $keys), $value, $repo[$key]);
+        $this->repo = array_replace_recursive($this->repo, $repo);
+        return $this;
+    }
+
+    /**
+     * Add a repository as a delegate.
+     * @param  RepositoryInterface $repository Repository interface
+     * @return $this Allow chaining
+     */
+    public function delegate($repository)
+    {
+        $this->delegates[] = $repository;
+        return $this;
+    }
+}
---
+++ tests/Repository_2_0_0_Test.php
@@ -0,0 +1,174 @@
+<?php
+
+use Boots\Repository_2_0_0 as Repository;
+
+define('ABSPATH', null);
+
+class Repository_2_0_0_Test extends PHPUnit_Framework_TestCase
+{
+    protected $repo;
+
+    protected $config;
+
+    protected function setUp()
+    {
+        $this->config = [
+            'one' => 'one_value',
+            'two' => [
+                'two_one' => 'two_one_value',
+                'two_two' => 'two_two_value',
+            ],
+            'three' => [
+                'three_one' => 'three_one_value',
+                'three_two' => [
+                    'three_two_one' => 'three_two_one_value',
+                ],
+                'three_three' => 'three_three_value',
+            ],
+        ];
+        $this->repo = new Repository($this->config);
+    }
+
+    /** @test */
+    public function it_should_be_instantiable_with_an_empty_array_by_default()
+    {
+        $repo = new Repository;
+        $this->assertEquals([], $repo->all());
+    }
+
+    /** @test */
+    public function it_should_be_instantiable_with_a_provided_array()
+    {
+        $this->assertEquals($this->config, $this->repo->all());
+    }
+
+    /** @test */
+    public function it_should_return_the_entire_repository_as_an_array()
+    {
+        $this->assertEquals($this->config, $this->repo->all());
+    }
+
+    /** @test */
+    public function it_should_determine_whether_a_given_key_exists()
+    {
+        $this->assertEquals(true, $this->repo->has('one'));
+        $this->assertEquals(false, $this->repo->has('one.two'));
+        $this->assertEquals(true, $this->repo->has('two'));
+        $this->assertEquals(true, $this->repo->has('two.two_one'));
+        $this->assertEquals(false, $this->repo->has('two.two_one.two_one_one'));
+        $this->assertEquals(true, $this->repo->has('two.two_two'));
+        $this->assertEquals(false, $this->repo->has('two.two_two.two_two_one'));
+        $this->assertEquals(false, $this->repo->has('two.three'));
+        $this->assertEquals(true, $this->repo->has('three.three_one'));
+        $this->assertEquals(true, $this->repo->has('three.three_two'));
+        $this->assertEquals(true, $this->repo->has('three.three_two.three_two_one'));
+        $this->assertEquals(true, $this->repo->has('three.three_three'));
+        $this->assertEquals(false, $this->repo->has('three.four'));
+        $this->assertEquals(false, $this->repo->has('foo'));
+        $this->assertEquals(false, $this->repo->has('foo.bar'));
+        $this->assertEquals(false, $this->repo->has('foo.bar.baz'));
+    }
+
+    /** @test */
+    public function it_should_return_the_value_for_a_given_key()
+    {
+        $this->assertEquals('one_value', $this->repo->get('one'));
+        $this->assertEquals('one_value', $this->repo->get('one', 'one'));
+        $this->assertEquals('two_one_value', $this->repo->get('two.two_one'));
+        $this->assertEquals('two_one_value', $this->repo->get('two.two_one', 'two'));
+        $this->assertEquals('three_two_one_value', $this->repo->get('three.three_two.three_two_one'));
+        $this->assertEquals('three_two_one_value', $this->repo->get('three.three_two.three_two_one', 'three'));
+    }
+
+    /** @test */
+    public function it_should_return_null_if_given_key_is_not_found()
+    {
+        $this->assertEquals(null, $this->repo->get('lorem'));
+        $this->assertEquals(null, $this->repo->get('lorem.ipsum'));
+        $this->assertEquals(null, $this->repo->get('one.two'));
+    }
+
+    /** @test */
+    public function it_should_return_default_value_if_given_key_is_not_found()
+    {
+        $this->assertEquals([], $this->repo->get('lorem', []));
+        $this->assertEquals(1, $this->repo->get('lorem.ipsum', 1));
+        $this->assertEquals(0, $this->repo->get('one.two', 0));
+    }
+
+    /** @test */
+    public function it_should_set_a_value_for_a_given_key()
+    {
+        $this->assertEquals(null, $this->repo->get('foo'));
+        $this->repo->set('foo', 'bar');
+        $this->assertEquals('bar', $this->repo->get('foo'));
+        $this->assertEquals('bar', $this->repo->get('foo', 'foo'));
+
+        $this->assertEquals(null, $this->repo->get('beep.boop'));
+        $this->repo->set('beep.boop', 'baz');
+        $this->assertEquals('baz', $this->repo->get('beep.boop'));
+        $this->assertEquals('baz', $this->repo->get('beep.boop', 'boop'));
+
+        $this->assertEquals('one_value', $this->repo->get('one'));
+        $this->repo->set('one', 'foo');
+        $this->assertEquals('foo', $this->repo->get('one'));
+        $this->assertEquals('foo', $this->repo->get('one', 'one'));
+
+        $this->assertEquals($this->config['two'], $this->repo->get('two'));
+        $this->repo->set('two', ['foo' => 'bar', 'bar' => 'baz']);
+        $this->assertEquals('two_one_value', $this->repo->get('two.two_one'));
+        $this->assertEquals('two_two_value', $this->repo->get('two.two_two'));
+        $this->assertEquals('bar', $this->repo->get('two.foo'));
+        $this->assertEquals('bar', $this->repo->get('two.foo', 'foo'));
+        $this->assertEquals('baz', $this->repo->get('two.bar'));
+        $this->assertEquals('baz', $this->repo->get('two.bar', 'bar'));
+    }
+
+    /** @test */
+    public function it_should_allow_delegation()
+    {
+        $this->assertEquals($this->config, $this->repo->all());
+        $config = [
+            'a' => 'a_value',
+            'b' => [
+                'b_a' => 'b_a_value',
+                'b_b' => 'b_b_value',
+            ],
+        ];
+        $this->repo->delegate(new Repository(['a' => $config['a']]));
+        $this->repo->delegate(new Repository(['b' => $config['b']]));
+
+        $this->assertEquals('one_value', $this->repo->get('one'));
+        $this->assertEquals('two_one_value', $this->repo->get('two.two_one'));
+        $this->assertEquals('two_two_value', $this->repo->get('two.two_two'));
+        $this->assertEquals('a_value', $this->repo->get('a'));
+        $this->assertEquals('b_a_value', $this->repo->get('b.b_a'));
+        $this->assertEquals('b_b_value', $this->repo->get('b.b_b'));
+
+        $this->assertEquals(true, $this->repo->has('one'));
+        $this->assertEquals(false, $this->repo->has('one.two'));
+        $this->assertEquals(true, $this->repo->has('two'));
+        $this->assertEquals(true, $this->repo->has('two.two_one'));
+        $this->assertEquals(false, $this->repo->has('two.two_one.two_one_one'));
+        $this->assertEquals(true, $this->repo->has('a'));
+        $this->assertEquals(true, $this->repo->has('b'));
+        $this->assertEquals(true, $this->repo->has('b.b_a'));
+        $this->assertEquals(true, $this->repo->has('b.b_b'));
+    }
+
+    /** @test */
+    public function it_should_return_the_entire_repository_with_delegates_as_an_array()
+    {
+        $this->repo->delegate(new Repository(['a' => 'b']));
+
+        $all = $this->repo->all(true);
+        $this->assertEquals(2, count($all));
+        $this->assertEquals($this->config, $all[0]);
+        $this->assertEquals(['a' => 'b'], $all[1]);
+
+        $all = $this->repo->everything();
+        $this->assertEquals(2, count($all));
+        $this->assertEquals($this->config, $all[0]);
+        $this->assertEquals(['a' => 'b'], $all[1]);
+    }
+}
