ad9a574c9cb06003d80b9a284c4f81268187470a
Kamal Khan <shout@bhittani.com>
2016-12-03 00:06:56 +0500

    Only check for .env file if ABSPATH isnt set.

--- tests/tests.php
+++ tests/tests.php
@@ -4,8 +4,10 @@ define('WP_USE_THEMES', false);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
-$dotenv->load();
-$dotenv->required('ABSPATH');
+if (!getenv('ABSPATH')) {
+    $dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+    $dotenv->load();
+    $dotenv->required('ABSPATH');
+}
 
 require rtrim(getenv('ABSPATH'), '/') . '/wp-load.php';
