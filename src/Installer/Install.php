cba065471faa46e666a57d6f37de97a8554c9d4c
Kamal Khan <shout@bhittani.com>
2016-12-02 17:37:08 +0500

    Create composer script for extension installation.

---
+++ src/Installer/Install.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Boots\Installer;
+
+/**
+ * Boots installer for composer.
+ *
+ * @package Boots
+ * @subpackage Installer\Install
+ * @version 2.0.0
+ * @see http://wpboots.com
+ * @link https://github.com/wpboots/boots
+ * @author Kamal Khan <shout@bhittani.com> https://bhittani.com
+ * @license https://github.com/wpboots/boots/blob/master/LICENSE
+ * @copyright Copyright (c) 2014-2016, Kamal Khan
+ */
+
+use PhpParser\Error;
+// use Composer\Script\Event;
+use PhpParser\NodeTraverser;
+use PhpParser\ParserFactory;
+use Composer\Installer\PackageEvent;
+use PhpParser\NodeVisitor\NameResolver;
+use PhpParser\PrettyPrinter\Standard as PhpPrinter;
+
+/**
+ * @package Boots
+ * @subpackage Installer\Install
+ * @version 2.0.0
+ */
+class Install
+{
+    protected static function mountExtension($version, $path2file, $path2manifest)
+    {
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        $traverser = new NodeTraverser;
+        $prettyPrinter = new PhpPrinter;
+        // resolve names
+        $traverser->addVisitor(new NameResolver);
+        // custom visitor
+        $visitor = new NodeVisitor($version);
+        $traverser->addVisitor($visitor);
+        // contents
+        $code = file_get_contents($path2file);
+        // parse
+        $stmts = $parser->parse($code);
+        // traverse
+        $stmts = $traverser->traverse($stmts);
+        // pretty print
+        $code = $prettyPrinter->prettyPrintFile($stmts);
+        // save file
+        file_put_contents($path2file, $code);
+        // save manifest
+        $fqcn = str_replace('\\', '\\\\', $visitor->getFqcn());
+        $manifest = "{\"class\":\"{$fqcn}\", \"version\":\"{$version}\"}";
+        file_put_contents($path2manifest, $manifest);
+    }
+
+    protected static function extension(PackageEvent $event, $package)
+    {
+        $type = $package->getType();
+        if ($type != 'boots-extension') {
+            return;
+        }
+        $composer = $event->getComposer();
+        $name = $package->getName();
+        $version = $package->getPrettyVersion();
+        $path = $composer->getInstallationManager()->getInstallPath($package);
+        $path2file = "{$path}/index.php";
+        $path2manifest = "{$path}/boots.json";
+        if (!is_file($path2manifest)) {
+            static::mountExtension($version, $path2file, $path2manifest);
+        }
+    }
+
+    public static function extensionInstall(PackageEvent $event)
+    {
+        $package = $event->getOperation()->getPackage();
+        static::extension($event, $package);
+    }
+
+    public static function extensionUpdate(PackageEvent $event)
+    {
+        $package = $event->getOperation()->getInitialPackage();
+        static::extension($event, $package);
+    }
+
+    public static function boots(PackageEvent $event)
+    {
+        //
+    }
+}
---
+++ src/Installer/NodeVisitor.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Boots\Installer;
+
+/**
+ * Node visitor for boots installer.
+ *
+ * @package Boots
+ * @subpackage Installer\NodeVisitor
+ * @version 2.0.0
+ * @see http://wpboots.com
+ * @link https://github.com/wpboots/boots
+ * @author Kamal Khan <shout@bhittani.com> https://bhittani.com
+ * @license https://github.com/wpboots/boots/blob/master/LICENSE
+ * @copyright Copyright (c) 2014-2016, Kamal Khan
+ */
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt;
+use PhpParser\NodeVisitorAbstract;
+
+/**
+ * @package Boots
+ * @subpackage Installer\NodeVisitor
+ * @version 2.0.0
+ */
+class NodeVisitor extends NodeVisitorAbstract
+{
+    protected $version;
+
+    protected $fqcn;
+
+    public function __construct($version)
+    {
+        $this->version = str_replace('.', '_', $version);
+    }
+
+    public function leaveNode(Node $node)
+    {
+        if ($node instanceof Stmt\Class_) {
+            $this->fqcn = $node->namespacedName->toString();
+            $node->name = "{$this->fqcn}_{$this->version}";
+        }
+    }
+
+    public function getFqcn()
+    {
+        return $this->fqcn;
+    }
+}
