c1fb8b8f7ee18b2bf3c26ffd2bb9aa44524cba4d
Kamal Khan <shout@bhittani.com>
2016-12-02 19:21:11 +0500

    wip

--- src/Installer/Install.php
+++ src/Installer/Install.php
@@ -88,10 +88,8 @@ class Install
     public static function boots(Event $event)
     {
         $composer = $event->getComposer();
-        $package = $event->getOperation()->getPackage();
-        $name = $package->getName();
-        $version = $package->getPrettyVersion();
-        $path = $composer->getInstallationManager()->getInstallPath($package);
-        dump($name, $version, $path);
+        $path = $composer->getPath();
+        $version = 'dev';
+        dump($version, $path);
     }
 }
