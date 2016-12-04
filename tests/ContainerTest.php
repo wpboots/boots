e0930e3bf4410daa54267a1644080df012694e32
Kamal Khan <shout@bhittani.com>
2016-12-05 03:23:46 +0500

    More test cases.

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -12,6 +12,11 @@ class ContainerTest extends PHPUnit_Framework_TestCase
     {
         vfsStream::setup('boots', null, [
             'container' => [
+                'Foobar.php' =>
+                    '<?php namespace Boots\Test\Container;
+                        class Foobar {
+                            public function __construct($foo) {}
+                        }',
                 'WithZeroParams.php' =>
                     '<?php namespace Boots\Test\Container;
                         class WithZeroParams {}',
@@ -25,13 +30,20 @@ class ContainerTest extends PHPUnit_Framework_TestCase
                         class WithTwoParams {
                             public function __construct(WithZeroParams $w0, WithOneParam $w1) {}
                         }',
+                'WithManagedParams.php' =>
+                    '<?php namespace Boots\Test\Container;
+                        class WithManagedParams {
+                            public function __construct(Foobar $foo, WithOneParam $w1) {}
+                        }',
                 //
             ],
         ]);
 
+        require_once vfsStream::url('boots/container/Foobar.php');
         require_once vfsStream::url('boots/container/WithZeroParams.php');
         require_once vfsStream::url('boots/container/WithOneParam.php');
         require_once vfsStream::url('boots/container/WithTwoParams.php');
+        require_once vfsStream::url('boots/container/WithManagedParams.php');
 
         $this->container = new Container(new Repository);
     }
@@ -60,6 +72,15 @@ class ContainerTest extends PHPUnit_Framework_TestCase
     public function it_should_resolve_an_object_if_class_constructor_params_can_be_resolved_recursively()
     {
         $class = 'Boots\Test\Container\WithTwoParams';
+        $this->assertInstanceOf($class, $this->container->get($class));
+    }
+
+    /** @test */
+    public function it_should_resolve_an_object_if_class_constructor_params_are_being_managed()
+    {
+        $class = 'Boots\Test\Container\WithManagedParams';
+        $foobarClass = 'Boots\Test\Container\Foobar';
+        $this->container->add($foobarClass, new $foobarClass('bar'));
         $this->assertInstanceOf($class, $this->container->get($class));
     }
 }
