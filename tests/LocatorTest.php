1664c2ed7efa33664703b1b6f091e5ea0fb7a2df
Kamal Khan <shout@bhittani.com>
2016-12-04 05:20:55 +0500

    Unit test 3 for locator.

--- tests/LocatorTest.php
+++ tests/LocatorTest.php
@@ -12,13 +12,14 @@ class LocatorTest extends PHPUnit_Framework_TestCase
         vfsStream::setup('boots', null, [
             'TestFooBar.php' => '<?php namespace Test\Foo; class Bar {}',
             'TestFooBar_1_0.php' => '<?php namespace Test\Foo; class Bar_1_0 {}',
+            'TestFooBar_1_0_.php' => '<?php namespace Test\Foo; class Bar_1_0_ {}',
         ]);
 
         $this->locator = new Locator;
     }
 
     /** @test */
-    public function it_should_load_a_class_from_a_file_if_class_not_found()
+    public function it_should_load_a_class_from_a_file_if_class_does_not_exist()
     {
         $filepath = vfsStream::url('boots/TestFooBar.php');
         $fqcn = $this->locator->locate($filepath, 'Test\Foo\Bar');
@@ -26,10 +27,22 @@ class LocatorTest extends PHPUnit_Framework_TestCase
     }
 
     /** @test */
-    public function it_should_load_a_versioned_class_from_a_file_if_class_not_found()
+    public function it_should_load_a_versioned_class_from_a_file_if_class_does_not_exist()
     {
         $filepath = vfsStream::url('boots/TestFooBar_1_0.php');
         $fqcn = $this->locator->locate($filepath, 'Test\Foo\Bar', '1.0');
         $this->assertEquals('Test\Foo\Bar_1_0', $fqcn);
+    }
+
+    /** @test */
+    public function it_should_return_versioned_fqcn_without_loading_file_if_class_exists()
+    {
+        $filepath = vfsStream::url('boots/TestFooBar_1_0.php');
+        $this->locator->locate($filepath, 'Test\Foo\Bar', '1.0');
+
+        $filepath = vfsStream::url('boots/TestFooBar_1_0_.php');
+        $fqcn = $this->locator->locate($filepath, 'Test\Foo\Bar', '1.0');
+        $this->assertEquals('Test\Foo\Bar_1_0', $fqcn);
+        $this->assertTrue(!class_exists('Test\Foo\Bar_1_0_'));
     }
 }
