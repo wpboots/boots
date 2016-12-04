1ad1709590af14c53b9f74b4cb0a8f88cb334de2
Kamal Khan <shout@bhittani.com>
2016-12-04 05:12:16 +0500

    Use vfs for test cases requiring file structure. Make locator tests pass.

--- src/Locator.php
+++ src/Locator.php
@@ -44,11 +44,15 @@ class Locator implements Contract\LocatorContract
         $versionedClass = $class . $suffix;
         if (!class_exists($versionedClass)) {
             if (!is_file($filepath)) {
-                throw new Exception\FileNotFoundException('File not found.');
+                throw new Exception\FileNotFoundException(sprintf(
+                    'File %s could not be found.', $filepath
+                ));
             }
             require_once $filepath;
             if (!class_exists($versionedClass)) {
-                throw new Exception\ClassNotFoundException('Class not found.');
+                throw new Exception\ClassNotFoundException(sprintf(
+                    'Class %s could not be found in %s.', $versionedClass, $filepath
+                ));
             }
         }
         return $versionedClass;
--- tests/LocatorTest.php
+++ tests/LocatorTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Boots\Locator;
+use org\bovigo\vfs\vfsStream;
 
 class LocatorTest extends PHPUnit_Framework_TestCase
 {
@@ -8,20 +9,27 @@ class LocatorTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        vfsStream::setup('boots', null, [
+            'TestFooBar.php' => '<?php namespace Test\Foo; class Bar {}',
+            'TestFooBar_1_0.php' => '<?php namespace Test\Foo; class Bar_1_0 {}',
+        ]);
+
         $this->locator = new Locator;
     }
 
     /** @test */
     public function it_should_load_a_class_from_a_file_if_class_not_found()
     {
-        $fqcn = $this->locator->locate(__DIR__, 'Foo\Bar');
-        $this->assertTrue('Foo\Bar', $fqcn);
+        $filepath = vfsStream::url('boots/TestFooBar.php');
+        $fqcn = $this->locator->locate($filepath, 'Test\Foo\Bar');
+        $this->assertEquals('Test\Foo\Bar', $fqcn);
     }
 
     /** @test */
     public function it_should_load_a_versioned_class_from_a_file_if_class_not_found()
     {
-        $fqcn = $this->locator->locate(__DIR__, 'Foo\Bar', '0.1');
-        $this->assertTrue('Foo\Bar_0_1', $fqcn);
+        $filepath = vfsStream::url('boots/TestFooBar_1_0.php');
+        $fqcn = $this->locator->locate($filepath, 'Test\Foo\Bar', '1.0');
+        $this->assertEquals('Test\Foo\Bar_1_0', $fqcn);
     }
 }
